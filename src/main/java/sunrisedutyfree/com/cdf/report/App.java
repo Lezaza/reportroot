588408b13085384168944a66e00c65b0f80a7386
谢林峻 <314037743@qq.com>
2018-07-27 13:46:18 +0800

    项目首建

---
+++ src/main/java/sunrisedutyfree/com/cdf/report/App.java
@@ -0,0 +1,13 @@
+package sunrisedutyfree.com.cdf.report;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
